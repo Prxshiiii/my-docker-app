7d6d2d15e623a783730c19a237743d4e394d8f05
Prxshiiii <prashanth.priv@gmail.com>
2026-05-14 04:29:23 +0000

    tested deployment automatically

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello from Docker!</h1><p>My first DevOps project.</p>"
+    return "<h1>Hello from Docker!</h1><p>Version 2 deployed automatically</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
